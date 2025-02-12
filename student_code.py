3d444e176e1e77af7ec2fbf213279a632b8eb0af
Ehsan Lak <151562034+Ehsunpy@users.noreply.github.com>
2025-02-12 22:10:13 +0330

    test.py

--- student_code.py
+++ student_code.py
@@ -1,6 +1,7 @@
-# student_code.py
-def square(a):
+from student_code import square
 
-    return a*a
-
-square(2)
+def test_square():
+    assert square(2) == 4
+    assert square(-3) == 9
+    assert square(0) == 0
+    assert square(5) == 25
